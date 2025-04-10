53e0418b32554820e88b2b63214529dd275e509d
abhishek <abhishek@raapid.ai>
2025-04-10 09:27:08 +0530

    Task 2: Added implementatioan and test for comma separated numbers

--- string_calculator.py
+++ string_calculator.py
@@ -3,3 +3,6 @@ def add(numbers):
 
     if not numbers:
         return 0
+
+    nums = numbers.split(",")
+    return sum(int(num) for num in nums)
--- test_calculator.py
+++ test_calculator.py
@@ -4,3 +4,9 @@ from string_calculator import add
 def test_empty_string_returns_zero():
     assert add("") == 0
 
+def test_single_number_returns_itself():
+    assert add("1") == 1
+
+def test_two_numbers_comma_separated():
+    assert add("1,2") == 3
+
