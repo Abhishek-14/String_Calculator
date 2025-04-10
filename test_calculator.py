ad86cb53c5cdc54cd7fc9caa39db86cbdf08adf3
abhishek <abhishek@raapid.ai>
2025-04-10 09:28:34 +0530

    Task 2: Added test for multiple comma separated numbers

--- test_calculator.py
+++ test_calculator.py
@@ -10,3 +10,7 @@ def test_single_number_returns_itself():
 def test_two_numbers_comma_separated():
     assert add("1,2") == 3
 
+def test_multiple_numbers_returns_sum():
+    assert add("1,2,3") == 6
+    assert add("4,5,6,7,8") == 30
+
